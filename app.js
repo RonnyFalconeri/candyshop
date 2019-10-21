bacbd324a86f32aef1925c9fcf74b8034cae8e0c
RonnyFalconeri <34677325+RonnyFalconeri@users.noreply.github.com>
2019-10-21 22:04:33 +0200

    projekt kaputt

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var app = express();
 
 // initialisations
 app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.get("/", function(req, res){
+    res.render("index");
+});
 app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.json());
 
