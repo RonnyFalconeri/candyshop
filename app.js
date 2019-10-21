7fb0610c99ca6bccadce0b640ab8e4bb43f3ba23
RonnyFalconeri <ronny@falconeri.de>
2019-10-21 11:12:03 +0200

    vorbereitung

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
   
-var express = require('express');
+// requirements
+var express = require("express");
+var path = require("path");
+var mysql = require("mysql");
+var bodyparser = require("body-parser");
 var app = express();
-var server = require('http').createServer(app);
-var port = 3000;
 
-server.listen(port, function () {
-    console.log('Webserver listening on port: %d', port);
+// initialisations
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+app.use(bodyparser.json());
+
+
+var db = false;
+
+if(db)
+{
+    // astablish connection variables of database
+    var mysqlConnection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "admin",
+        database: "candyshop"
+    });
+
+    // connect to database
+    mysqlConnection.connect((err) => {
+        if(!err)
+            console.log("Database: connection successful!");
+        else
+            console.log("Database: connection failed. Error: "+ JSON.stringify(err, undefined, 2));
+    });
+}else {
+    console.log("Database is disabled.");
+}
+
+
+// start webserver -------------------------------
+app.listen(app.get("port"), function(){
+    console.log("CandyShop | Server started on port "+ app.get("port"));
 });
 
-app.use(express.static(__dirname + '/public'));
+
+
+// --------------------------------- Requests -------------------------------
+
+// Beispiel einer request
+// return kunde with given id
+app.get("/kunde/id/:id", (req, res) =>{
+    mysqlConnection.query("SELECT * FROM kunde WHERE id=?", [req.params.id], (err, rows, fields) =>{
+        if (err) throw err;
+        res.send(rows);
+    });
+});
