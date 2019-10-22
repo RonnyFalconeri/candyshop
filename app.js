aa975c8c0374a30932eb5d8ee5505ca2af8fb1b7
max <max>
2019-10-22 15:56:59 +0200

    ip adress

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ if(db)
     // astablish connection variables of database
     var mysqlConnection = mysql.createConnection({
         host: "localhost",
-        port: 3306,
         user: "root",
         password: "",
         database: "candyshop"
